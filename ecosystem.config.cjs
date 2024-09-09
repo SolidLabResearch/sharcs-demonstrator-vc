56da04e4aa1d0ff4e8472af5b144f3b5e04a254d
Gertjan De Mulder <gertjan.demulder@ugent.be>
2024-09-09 18:48:33 +0200

    update: added gateway service to pm2 config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,5 +1,6 @@
 module.exports = {
   apps : [
+      { name: "gateway", script: 'src/services/gateway.js'},
       { name: "registry", script: 'src/services/registry.js'},
       { name: "deriver", script: 'src/services/deriver.js' }
   ]
