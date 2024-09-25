81adb46ec0e48692e1f42f738bac76f6939116df
Gertjan De Mulder <gertjan.demulder@ugent.be>
2024-09-25 21:37:13 +0200

    Fix: Jest could not find tests anymore

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,6 @@ export default {
   transform: {
     "^.+\\.js$": "babel-jest",
   },
-  testPathIgnorePatterns: ["actors.js"],
-  modulePathIgnorePatterns: ["temp"],
   testEnvironment: "node",
   verbose: true,
   testTimeout: 3000,
